6359af823a757a4948b8d251b9df1e5be0d3a03c
Diego Galvan <diego.galvan117@gmail.com>
2025-10-15 18:40:18 -0600

    Dark mode toggle. Theme is saved in localStorage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import DarkModeToggle from "./components/DarkModeToggle";
 import Stopwatch from "./components/Stopwatch";
 
 function App() {
   return (
-    <div>
+    <div className="app h-screen dark:bg-neutral-900 select-none">
+      <DarkModeToggle />
       <Stopwatch />
     </div>
   );
