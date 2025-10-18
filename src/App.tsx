11e025f8aa542ffb08b7cd1cecb0f88650a7ffb4
dgoglvn <diego.galvan117@gmail.com>
2025-10-18 16:30:26 -0600

    lifted the React state [time, setTime] and [isRunning, setIsRunning] to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import DarkModeToggle from "./components/DarkModeToggle";
-import Stopwatch from "./components/StopwatchComponent";
+import StopwatchComponent from "./components/StopwatchComponent";
+import { Stopwatch } from "./classes/Stopwatch";
 
 function App() {
-  const [totalTime, setTotalTime] = useState<number>(0);
+  const stopwatchRef = useRef<Stopwatch>(new Stopwatch());
 
-  const resetTime = (x: boolean) => {
-    if (x) {
-      console.log("true");
-      setTotalTime(0);
-    }
+  const [time, setTime] = useState<number>(stopwatchRef.current.count);
+  const [isRunning, setIsRunning] = useState<boolean>(
+    stopwatchRef.current.isRunning
+  );
+
+  useEffect(() => {
+    const sw = stopwatchRef.current!;
+    const unsubscribe = sw.subscribe(setTime);
+
+    return () => {
+      unsubscribe();
+      sw.stop();
+    };
+  }, []);
+
+  const handleStart = (): void => {
+    stopwatchRef.current!.start();
+    setIsRunning(stopwatchRef.current.isRunning);
+  };
+
+  const handleStop = (): void => {
+    stopwatchRef.current!.stop();
+    setIsRunning(stopwatchRef.current.isRunning);
+  };
+
+  const handleReset = (): void => {
+    stopwatchRef.current.reset();
+    setTime(0);
+    setIsRunning(false);
   };
 
   return (
     <div className="app h-screen dark:bg-neutral-900 select-none">
-      <DarkModeToggle totalTime={totalTime} resetTime={resetTime} />
-      <Stopwatch />
+      <DarkModeToggle time={time} handleReset={handleReset} />
+      <StopwatchComponent
+        time={time}
+        isRunning={isRunning}
+        handleStart={handleStart}
+        handleStop={handleStop}
+      />
     </div>
   );
 }
