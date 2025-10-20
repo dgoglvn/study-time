5a8b19e2d19eeabf108c9a8150495d904100adc0
dgoglvn <diego.galvan117@gmail.com>
2025-10-20 11:35:09 -0600

    added Toastify to show the user the total time spent. minor style tweaks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import { toast, ToastContainer, Bounce } from "react-toastify";
 import DarkModeToggle from "./components/DarkModeToggle";
 import StopwatchComponent from "./components/StopwatchComponent";
 import { Stopwatch } from "./classes/Stopwatch";
@@ -10,7 +11,7 @@ function App() {
 
   const [time, setTime] = useState<number>(stopwatchRef.current.count);
   const [isRunning, setIsRunning] = useState<boolean>(
-    stopwatchRef.current.isRunning
+    stopwatchRef.current.isRunning,
   );
 
   useEffect(() => {
@@ -23,12 +24,13 @@ function App() {
     };
   }, []);
 
-  // useEffect(() => {
-  //   if (!isRunning && time !== 0) {
-  //     localStorage.setItem("timeStr", formatHMMSS(time));
-  //     localStorage.setItem("count", time.toString());
-  //   }
-  // }, [isRunning, time]);
+  useEffect(() => {
+    if (time > 0) {
+      document.title = formatHMMSS(time);
+    } else {
+      document.title = "Study Time";
+    }
+  });
 
   const handleStart = (): void => {
     stopwatchRef.current!.start();
@@ -44,7 +46,11 @@ function App() {
     stopwatchRef.current.reset();
     setTime(0);
     setIsRunning(false);
+    notifyTotalTime();
   };
+
+  const notifyTotalTime = () =>
+    toast(`Your time spent studying: ${formatHMMSS(time)}`);
 
   return (
     <div className="app h-screen font-geist-mono dark:bg-neutral-900 select-none">
@@ -54,6 +60,19 @@ function App() {
         isRunning={isRunning}
         handleStart={handleStart}
         handleStop={handleStop}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme={localStorage.getItem("theme") === "light" ? "light" : "dark"}
+        transition={Bounce}
       />
     </div>
   );
--- src/components/StopwatchComponent.tsx
+++ src/components/StopwatchComponent.tsx
@@ -13,13 +13,9 @@ const StopwatchComponent = ({
   handleStart,
   handleStop,
 }: ChildProps) => {
-  // const savedFormattedTime = localStorage.getItem("timeStr") || null;
-  // const savedCount = Number(localStorage.getItem("count") || null);
-
   return (
-    <div className="flex flex-col mx-auto h-11/12 items-center justify-center dark:text-dark-white">
+    <div className="flex flex-col mx-auto h-10/12 items-center justify-center dark:text-dark-white">
       <p className="text-centert tabular-nums lining-nums tracking-tight text-[clamp(6rem,20vw,30rem)] font-medium dark:text-dark-white fixed-width-digits">
-        {/* {savedCount !== 0 ? savedFormattedTime : formatHMMSS(time)} */}
         {formatHMMSS(time)}
       </p>
       <div className="flex items-center mx-auto">
@@ -30,7 +26,6 @@ const StopwatchComponent = ({
               ? "text-white px-6 py-3 sm:px-8 sm:py-4 sm:h-14 min-w-[9rem] text-[clamp(1rem,4vw,1.25rem)] bg-blue-600 hover:bg-blue-700 font-semibold tracking-tight rounded-lg dark:bg-blue-500 dark:hover:bg-blue-600 dark:text-dark-white focus:outline-none cursor-pointer stopwatch-btn active"
               : "text-white px-6 py-3 sm:px-8 sm:py-4 sm:h-14 min-w-[9rem] text-[clamp(1rem,4vw,1.25rem)] bg-blue-600 hover:bg-blue-700 font-semibold tracking-tight rounded-lg dark:bg-blue-500 dark:hover:bg-blue-600 dark:text-dark-white focus:outline-none cursor-pointer stopwatch-btn"
           }
-          // className="text-white px-6 py-3 sm:px-8 sm:py-4 sm:h-14 min-w-[9rem] text-[clamp(1rem,4vw,1.25rem)] bg-blue-600 hover:bg-blue-700 font-semibold tracking-tight rounded-2xl dark:bg-blue-500 dark:hover:bg-blue-600 dark:text-dark-white focus:outline-none active:scale-[.98] cursor-pointer stopwatch-btn"
           onClick={() => {
             if (isRunning) handleStop();
             else handleStart();
